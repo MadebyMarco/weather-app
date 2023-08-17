35c5e5cacb01961be0768298b11fb39df2f6d643
MadebyMarco <marco3xmedina@gmail.com>
2023-08-17 12:06:00 -0700

    func: getProcessedWeather

--- script.js
+++ script.js
@@ -8,7 +8,52 @@ async function getWeatherOf(location) {
   return weather;
 }
 
-getWeatherOf("tokyo").then((data) => console.log(data));
+function getProcessedWeather(
+  locationName,
+  locationCountry,
+  feelsLikeF,
+  feelsLikeC,
+  cloudConditionText,
+  cloudConditionIcon,
+  windMPH,
+  temperatureF,
+  temperatureC
+) {
+  return {
+    locationName,
+    locationCountry,
+    feelsLikeF,
+    feelsLikeC,
+    cloudConditionText,
+    cloudConditionIcon,
+    windMPH,
+    temperatureF,
+    temperatureC,
+  };
+}
+
+function propagateWeatherDisplay(location) {
+  if (typeof location !== "string") {
+    console.error("Please enter a valid location");
+    return;
+  }
+}
+
+let processedWeather;
+getWeatherOf("San Diego").then((weather) => {
+  processedWeather = getProcessedWeather(
+    weather.location.name,
+    weather.location.country,
+    weather.current.feelslike_f,
+    weather.current.feelslike_c,
+    weather.current.condition.text,
+    weather.current.condition.icon,
+    weather.current.wind_mph,
+    weather.current.temp_f,
+    weather.current.temp_c
+  );
+  console.log(processedWeather);
+});
 
 //condition.text
 //location.name
